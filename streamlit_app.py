806a094ab19f0aa2d06530947129843991f01833
Tyler Hillery <hilleryty@yahoo.com>
2022-05-28 14:37:09 -0500

    initial commit
    provisioned postgres instance on AWS RDS and establish connection

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import psycopg2
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from learninglog.history;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
